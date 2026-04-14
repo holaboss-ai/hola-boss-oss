c10fbfd9c18042c5a2186c26f58caeca80894e47
jotyy <jotyy318@gmail.com>
2026-04-14 12:27:30 -0400

    fix(docs): redirect /docs url

--- website/docs/src/worker.ts
+++ website/docs/src/worker.ts
@@ -9,6 +9,12 @@ interface Env {
 export default {
   fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
+
+    if (url.pathname === "/docs") {
+      url.pathname = "/docs/";
+      return Response.redirect(url.toString(), 301);
+    }
+
     const rewrittenPath = url.pathname.replace(/^\/docs(?=\/|$)/, "") || "/";
 
     url.pathname = rewrittenPath;
